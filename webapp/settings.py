f1f784b267a4372d250a09616ee98f1b33aa0f38
Eduardo <esampaio@tecgraf.puc-rio.br>
2019-10-31 10:30:41 -0300

    ajustes e implementando leitura do arquivo cv

--- webapp/settings.py
+++ webapp/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'webapp.urls'
 
--- webapp/views.py
+++ webapp/views.py
@@ -7,4 +7,12 @@ from django.template import loader
 
 
 def index(request):
+    if request.method == "POST":
+        csv_file = request.FILES["arquivo"]
+        reader = csv.reader(csv_file) 
+        read_file(reader)
     return render(request, 'webapp/index.html')
+
+def read_file(reader):
+    for row in reader :
+        print (row)
