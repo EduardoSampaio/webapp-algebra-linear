1e1e9466b022ab5eae5f756ecb49d81375069661
Eduardo Sampaio <edwardviana@gmail.com>
2019-10-30 21:17:48 -0300

    ajuste de diretorios

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,10 @@
-#importing loading from django template  
-from django.template import loader  
-# Create your views here.  
-from django.http import HttpResponse  
+import codecs
+import csv
 
-def index(request):  
-   template = loader.get_template('index.html') # getting our template  
-   return HttpResponse(template.render())       # rendering the template in HttpResponse  
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+
+def index(request):
+    return render(request, 'webapp/index.html')
