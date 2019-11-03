dedf8692e04f2607fdcba2d3ab49985502cb1997
Eduardo Sampaio <edwardviana@gmail.com>
2019-11-03 02:02:10 -0300

    importação de arquivo csv e carregamento na tela

--- webapp/models/ManipularArquivo.py
+++ webapp/models/ManipularArquivo.py
@@ -1,12 +1,16 @@
+import numpy as np
 import csv
+import io
 
 
 class ManipularArquivo:
 
-    def __init__(self, request):
+    def __init__(self,request):
         self.request = request
 
-    def ler(self, request):
-        csv_file = request.FILES["docfile"]
-        reader = csv.reader(csv_file)
-        return None
+    def ler(self):
+      data = self.request.FILES['file'].read().decode('UTF-8')
+      io_string = io.StringIO(data)
+      next(io_string)
+      matriz = np.loadtxt(io_string, delimiter=',', dtype=int)
+      return matriz
--- webapp/views.py
+++ webapp/views.py
@@ -1,13 +1,24 @@
 
 
-
 from .models import ManipularArquivo
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
+import numpy as np;
+import csv
+import io
 
 def index(request):
-    if request.method == "POST":
-       arquivo = ManipularArquivo(request)
-       arquivo.ler()
+    if request.method == 'POST':
+        try:
+            data = request.FILES['file'].read().decode('UTF-8')
+            io_string = io.StringIO(data)         
+            next(io_string)
+            matriz = np.loadtxt(io_string, delimiter=',', dtype=int)
+            return render(request, 'index.html', {'matriz': matriz})
+        except:
+            print("ERRO")
+      
     return render(request, 'index.html')
+
+
