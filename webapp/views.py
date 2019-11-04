05fc08ca6719d0aaa43ebc7fb701d5c674f78556
Eduardo <esampaio@tecgraf.puc-rio.br>
2019-11-04 08:51:23 -0300

    ajuste na importação

--- webapp/views.py
+++ webapp/views.py
@@ -4,20 +4,15 @@ from .models import ManipularArquivo
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-import numpy as np;
+import numpy as np
 import csv
 import io
 
+
 def index(request):
     if request.method == 'POST':
-        try:
-            data = request.FILES['file'].read().decode('UTF-8')
-            io_string = io.StringIO(data)         
-            next(io_string)
-            matriz = np.loadtxt(io_string, delimiter=',', dtype=int)
-            return render(request, 'index.html', {'matriz': matriz})
-        except:
-            print("ERRO")  
+        data = request.FILES['file'].read().decode('UTF-8')
+        io_string = io.StringIO(data)
+        matriz = np.loadtxt(io_string, delimiter=',', dtype=int)
+        return render(request, 'index.html', {'matriz': matriz})
     return render(request, 'index.html')
-
-
